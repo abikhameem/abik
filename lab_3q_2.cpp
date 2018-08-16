5fbfc13f6ac06df3d7d30763c89593ed174118c5
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-16 21:38:58 +0530

    Add files via upload

---
+++ lab_3q_2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//write the main function
+int main(){int a,b,sum,difference,multiple,modulus;
+a=6;             //giving values to variables
+b=2;       
+sum=a+b;       //doing sum
+difference=a-b;    //doing difference
+multiply=a*b;
+float division=(a/b);
